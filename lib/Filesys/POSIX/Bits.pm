e0465be0d3daa7c9c8d2ba2a7f78fd2e1233467c
Erin Schoenhals <erin@cpanel.net>
2010-10-13 18:02:32 -0500

    Use Exporter in Filesys::POSIX::Bits

--- lib/Filesys/POSIX/Bits.pm
+++ lib/Filesys/POSIX/Bits.pm
@@ -1,70 +1,88 @@
+package Filesys::POSIX::Bits;
+
 use strict;
 use warnings;
+
+BEGIN {
+    use Exporter ();
+    use vars qw/@ISA @EXPORT/;
+
+    @ISA    = qw/Exporter/;
+
+    @EXPORT = qw(
+        $O_RDONLY $O_WRONLY $O_RDWR $O_NONBLOCK $O_APPEND $O_CREAT $O_TRUNC
+        $O_EXCL $O_SHLOCK $O_EXLOCK $O_NOFOLLOW $O_SYMLINK $O_EVTONLY $S_IFMT
+        $S_IFIFO $S_IFCHR $S_IFDIR $S_IFBLK $S_IFREG $S_IFLNK $S_IFSOCK
+        $S_IFWHT $S_IPROT $S_ISUID $S_ISGID $S_ISVTX $S_IPERM $S_IRWXU
+        $S_IRUSR $S_IWUSR $S_IXUSR $S_IRWXG $S_IRGRP $S_IWGRP $S_IXGRP
+        $S_IRWXO $S_IROTH $S_IWOTH $S_IXOTH
+    );
+}
 
 #
 # Flags as recognized by open()
 #
-our $O_RDONLY   = 0x0001;
-our $O_WRONLY   = 0x0002;
-our $O_RDWR     = 0x0004;
-our $O_NONBLOCK = 0x0008;
-our $O_APPEND   = 0x0010;
-our $O_CREAT    = 0x0020;
-our $O_TRUNC    = 0x0040;
-our $O_EXCL     = 0x0080;
-our $O_SHLOCK   = 0x0100;
-our $O_EXLOCK   = 0x0200;
-our $O_NOFOLLOW = 0x0400;
-our $O_SYMLINK  = 0x0800;
-our $O_EVTONLY  = 0x1000;
+my $O_RDONLY   = 0x0001;
+my $O_WRONLY   = 0x0002;
+my $O_RDWR     = 0x0004;
+my $O_NONBLOCK = 0x0008;
+my $O_APPEND   = 0x0010;
+my $O_CREAT    = 0x0020;
+my $O_TRUNC    = 0x0040;
+my $O_EXCL     = 0x0080;
+my $O_SHLOCK   = 0x0100;
+my $O_EXLOCK   = 0x0200;
+my $O_NOFOLLOW = 0x0400;
+my $O_SYMLINK  = 0x0800;
+my $O_EVTONLY  = 0x1000;
 
 #
 # Inode format bitfield and values
 #
-our $S_IFMT     = 0170000;
+my $S_IFMT     = 0170000;
 
-our $S_IFIFO    = 0010000;
-our $S_IFCHR    = 0020000;
-our $S_IFDIR    = 0040000;
-our $S_IFBLK    = 0060000;
-our $S_IFREG    = 0100000;
-our $S_IFLNK    = 0120000;
-our $S_IFSOCK   = 0140000;
-our $S_IFWHT    = 0160000;
+my $S_IFIFO    = 0010000;
+my $S_IFCHR    = 0020000;
+my $S_IFDIR    = 0040000;
+my $S_IFBLK    = 0060000;
+my $S_IFREG    = 0100000;
+my $S_IFLNK    = 0120000;
+my $S_IFSOCK   = 0140000;
+my $S_IFWHT    = 0160000;
 
 #
 # Inode execution protection bitfield and values
 #
-our $S_IPROT    = 0007000;
+my $S_IPROT    = 0007000;
 
-our $S_ISUID    = 0004000;
-our $S_ISGID    = 0002000;
-our $S_ISVTX    = 0001000;
+my $S_ISUID    = 0004000;
+my $S_ISGID    = 0002000;
+my $S_ISVTX    = 0001000;
 
 #
 # Inode permission bitfield and values
 #
-our $S_IPERM    = 0000777;
+my $S_IPERM    = 0000777;
 
 # Per assigned user
-our $S_IRWXU    = 0000700;
+my $S_IRWXU    = 0000700;
 
-our $S_IRUSR    = 0000400;
-our $S_IWUSR    = 0000200;
-our $S_IXUSR    = 0000100;
+my $S_IRUSR    = 0000400;
+my $S_IWUSR    = 0000200;
+my $S_IXUSR    = 0000100;
 
 # Per assigned group
-our $S_IRWXG    = 0000070;
+my $S_IRWXG    = 0000070;
 
-our $S_IRGRP    = 0000040;
-our $S_IWGRP    = 0000020;
-our $S_IXGRP    = 0000010;
+my $S_IRGRP    = 0000040;
+my $S_IWGRP    = 0000020;
+my $S_IXGRP    = 0000010;
 
 # All other users
-our $S_IRWXO    = 0000007;
+my $S_IRWXO    = 0000007;
 
-our $S_IROTH    = 0000004;
-our $S_IWOTH    = 0000002;
-our $S_IXOTH    = 0000001;
+my $S_IROTH    = 0000004;
+my $S_IWOTH    = 0000002;
+my $S_IXOTH    = 0000001;
 
 1;
