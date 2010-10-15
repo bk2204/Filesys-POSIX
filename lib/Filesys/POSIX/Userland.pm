8acfed55a6faf75522710bee90f9316875a3cded
Erin Schoenhals <erin@cpanel.net>
2010-10-15 12:29:25 -0500

    Initial implementation of Filesys::POSIX::Userland

---
+++ lib/Filesys/POSIX/Userland.pm
@@ -0,0 +1,65 @@
+package Filesys::POSIX;
+
+use strict;
+use warnings;
+
+use Filesys::POSIX::Bits;
+
+sub _find_inode_path {
+    my ($self, $start) = @_;
+    my $node = $start;
+    my @ret;
+
+    while (my $dir = $node->{'parent'}) {
+        last if $dir eq $node;
+
+        die('Not a directory') unless $dir->{'mode'} & $S_IFDIR;
+
+        dirent: foreach (keys %{$dir->{'dirent'}}) {
+            next if $_ eq '.' || $_ eq '..';
+            next dirent unless $dir->{'dirent'}->{$_} == $node;
+
+            push @ret, $_;
+            $node = $dir;
+        }
+    }
+
+    return '/' . join('/', reverse @ret);
+}
+
+sub getcwd {
+    my ($self) = @_;
+
+    return $self->_find_inode_path($self->{'cwd'});
+}
+
+sub realpath {
+    my ($self, $path) = @_;
+    my $node = $self->stat($path);
+
+    return $self->_find_inode_path($node);
+}
+
+sub mount {
+    my ($self, $fs, $path, %opts) = @_;
+    my $mountpoint = $self->stat($path);
+    my $realpath = $self->_find_inode_path($mountpoint);
+
+    $self->{'vfs'}->mount($fs, $realpath, $mountpoint, %opts);
+}
+
+sub unmount {
+    my ($self, $path) = @_;
+    my $mountpoint = $self->stat($path);
+
+    $self->{'vfs'}->unmount($mountpoint);
+}
+
+sub statfs {
+    my ($self, $path) = @_;
+    my $node = $self->stat($path);
+
+    return $self->{'vfs'}->statfs($node);
+}
+
+1;
