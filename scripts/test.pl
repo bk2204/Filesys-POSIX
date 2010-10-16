ca81331099ddc2d1c006d5224421d921d22e2e61
Erin Schoenhals <erin@cpanel.net>
2010-10-15 19:47:37 -0500

    Initial commit of scripts/test.pl

--- scripts/test.pl
+++ scripts/test.pl
@@ -3,12 +3,29 @@
 use strict;
 use warnings;
 
+use Filesys::POSIX;
+use Filesys::POSIX::Bits;
 use Filesys::POSIX::Mem;
+use Filesys::POSIX::Real;
+use Filesys::POSIX::Userland;
 
 use Data::Dumper;
+$Data::Dumper::Indent = 1;
 
-my $fs = Filesys::POSIX::Mem->new;
-$fs->mkdir('foo');
-$fs->chdir('foo/..');
+my $fs = Filesys::POSIX->new(Filesys::POSIX::Mem->new,
+    'noatime' => 1
+);
 
-printf("File descriptors: %s\n", join(', ', keys %{$fs->{'fds'}}));
+my $real = Filesys::POSIX::Real->new('/Users/erin');
+
+$fs->mkdir('/mnt');
+$fs->mkdir('/mnt/real');
+$fs->mount($real, '/mnt/real');
+$fs->unmount('/mnt/real');
+$fs->mount($real, '/mnt/real');
+
+foreach ($fs->mountpoints) {
+    my $dev = $fs->statfs($_);
+
+    printf("%s on %s (%s)\n", $dev, $_, join(', ', keys %{$dev->{'flags'}}));
+}
