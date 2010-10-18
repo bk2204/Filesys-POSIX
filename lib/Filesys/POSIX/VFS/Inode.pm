46306241ff93329b3d52650ac187f350d080015d
Erin Schoenhals <erin@cpanel.net>
2010-10-18 18:43:51 -0500

    Initial implementation of Filesys::POSIX::VFS::Inode

---
+++ lib/Filesys/POSIX/VFS/Inode.pm
@@ -0,0 +1,15 @@
+package Filesys::POSIX::VFS::Inode;
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, $mountpoint, $root) = @_;
+
+    return bless {
+        %$root,
+        'parent' => $mountpoint->{'parent'}
+    }, ref $root;
+}
+
+1;
