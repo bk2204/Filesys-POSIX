db47984306c9f7274a1ee70b69960feacf2bdd3a
Erin Schoenhals <kairi@heavyarms.local>
2010-10-19 01:19:42 -0500

    Refactored Filesys::POSIX::FdTable to allow it to dispatch device-specific file handles; keep records of file descriptor flags, inodes, and underlying file I/O handles within

--- lib/Filesys/POSIX/FdTable.pm
+++ lib/Filesys/POSIX/FdTable.pm
@@ -7,34 +7,45 @@ sub new {
     return bless {}, shift;
 }
 
-sub alloc {
-    my ($self, $inode) = @_;
+sub open {
+    my ($self, $inode, $flags) = @_;
     my $fd = 3;
+
+    my $handle = $inode->open($flags) or die('Unable to open device-specific file handle');
 
     foreach (sort { $a <=> $b } keys %$self) {
         next if $self->{$fd = $_ + 1};
         last;
     }
 
-    $self->{$fd} = $inode;
+    $self->{$fd} = {
+        'inode'     => $inode,
+        'handle'    => $handle,
+        'flags'     => $flags
+    };
 
     return $fd;
 }
 
 sub lookup {
     my ($self, $fd) = @_;
-    my $inode = $self->{$fd} or die('Invalid file descriptor');
+    my $entry = $self->{$fd} or die('Invalid file descriptor');
 
-    return $inode;
+    return $entry;
 }
 
-sub free {
+sub close {
     my ($self, $fd) = @_;
+    my $entry = $self->{$fd} or return;
+
+    $entry->{'handle'}->close;
+
     delete $self->{$fd};
 }
 
 sub list {
     my ($self) = @_;
+
     return keys %$self;
 }
 
