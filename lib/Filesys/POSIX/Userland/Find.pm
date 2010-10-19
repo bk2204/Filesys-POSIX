d096ac0cc64564af231129da6a685f814d167dda
Erin Schoenhals <erin@cpanel.net>
2010-10-19 15:54:56 -0500

    Initial commit of Filesys::POSIX::Userland::Find

---
+++ lib/Filesys/POSIX/Userland/Find.pm
@@ -0,0 +1,34 @@
+package Filesys::POSIX;
+
+use Filesys::POSIX::Bits;
+use Filesys::POSIX::Path;
+
+use strict;
+use warnings;
+
+sub find {
+    my ($self, $callback, @args) = @_;
+    my %opts;
+
+    if (ref $args[0] eq 'HASH') {
+        %opts = %{shift @args};
+    }
+
+    my @paths = map { Filesys::POSIX::Path->new($_) } @args;
+
+    while (my $path = pop @paths) {
+        my $node = $self->lstat($path->full);
+
+        $callback->($path);
+
+        if (($node->{'mode'} & $S_IFMT) == $S_IFDIR) {
+            push @paths, map {
+                Filesys::POSIX::Path->new($path->full . "/$_")
+            } grep {
+                $_ ne '.' && $_ ne '..'
+            } $node->{'dirent'}->list;
+        }
+    }
+}
+
+1;
