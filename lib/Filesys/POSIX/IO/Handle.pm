53ef32fd99b3b7d7f2b548079ff9752abaa808ec
Erin Schoenhals <erin@cpanel.net>
2010-10-20 18:15:14 -0500

    Initial implementation of Filesys::POSIX::IO::Handle

---
+++ lib/Filesys/POSIX/IO/Handle.pm
@@ -0,0 +1,43 @@
+package Filesys::POSIX::IO::Handle;
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, $fh) = @_;
+
+    return bless \$fh, $class;
+}
+
+sub write {
+    my ($self, $buf, $len) = @_;
+
+    return syswrite($$self, $buf, $len);
+}
+
+sub read {
+    my $self = shift;
+    my $len = pop;
+
+    return sysread($$self, $_[0], $len);
+}
+
+sub seek {
+    my ($self, $pos, $whence) = @_;
+
+    return sysseek($$self, $pos, $whence);
+}
+
+sub tell {
+    my ($self) = @_;
+
+    return tell($$self);
+}
+
+sub close {
+    my ($self) = @_;
+
+    close $$self;
+}
+
+1;
