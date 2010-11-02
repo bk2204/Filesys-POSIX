58be010bd949e07e00be1ef2affe3b96717c6921
Erin Schoenhals <erin@cpanel.net>
2010-11-02 16:50:25 -0500

    Added perldoc for Filesys::POSIX::Mem

--- lib/Filesys/POSIX/Mem.pm
+++ lib/Filesys/POSIX/Mem.pm
@@ -7,6 +7,27 @@ use Filesys::POSIX::Bits;
 use Filesys::POSIX::Mem::Inode;
 use Filesys::POSIX::Mem::Dirent;
 
+=head1 NAME
+
+Filesys::POSIX::Mem
+
+=head1 DESCRIPTION
+
+Provides the base class representing an instance of the memory file system.
+All inode and directory entry data lives completely in memory, or in temporary
+files referenced by inode buckets as provided by Filesys::POSIX::Mem::Bucket
+file handles.
+
+=head1 CREATING AND INITIALIZING THE FILESYSTEM
+
+=over
+
+=item $fs->new()
+
+Creates a new, unmounted filesystem.  The root inode of this filesystem is
+created as a directory with 0755 permissions.
+
+=cut
 sub new {
     my ($class) = @_;
     my $fs = bless {}, $class;
@@ -19,10 +40,17 @@ sub new {
     return $fs;
 }
 
-sub init {
-    my ($self, %data) = @_;
+=item $fs->init(%flags)
 
-    $self->{'flags'} = \%data;
+Stores the flags passed in the current filesystem object.  This method is
+usually called by L<Filesys::POSIX::Mount> when said filesystem object is
+mounted.  These mount flags can be retrieved later.
+
+=cut
+sub init {
+    my ($self, %flags) = @_;
+
+    $self->{'flags'} = \%flags;
 
     return $self;
 }
