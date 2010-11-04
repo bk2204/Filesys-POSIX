4b3eeed9372a118a3f1b58b1fd4664a36a71d51e
Erin Schoenhals <erin@cpanel.net>
2010-11-04 16:34:58 -0500

    Wrote POD for Filesys::POSIX::Userland::Find

--- lib/Filesys/POSIX/Userland/Find.pm
+++ lib/Filesys/POSIX/Userland/Find.pm
@@ -10,6 +10,65 @@ sub EXPORT {
     qw/find/;
 }
 
+=head1 NAME
+
+Filesys::POSIX::Userland::Find
+
+=head1 SYNOPSIS
+
+    use Filesys::POSIX;
+    use Filesys::POSIX::Real;
+
+    my $fs = Filesys::POSIX->new(Filesys::POSIX::Real->new,
+        'special'   => 'real:/home/foo',
+        'noatime'   => 1
+    );
+
+    $fs->find(sub {
+        my ($path, $inode) = @_;
+        printf("0%o %s\n", $inode->{'mode'}, $path->full);
+    }, '/');
+
+=head1 DESCRIPTION
+
+Filesys::POSIX::Userland::Find provides an extension module to Filesys::POSIX
+that operates very similarly in principle to the Perl Core module
+L<File::Find>, albeit with some minor differences and fewer options.  For the
+sake of efficiency, tail recursion, rather than pure call recursion, is used to
+handle very deep hierarchies.
+
+=head1 USAGE
+
+=over
+
+=item $fs->find($callback, @paths)
+
+=item $fs->find($callback, $options, @paths)
+
+$fs->find() will perform recursive descent into each path passed, printing the
+full pathname of each item found relative to each item found in the @paths
+list.  For each item found, both a Filesys::POSIX::Path object, and an inode,
+respectively, are passed as the sole arguments to the callback.  With this
+mechanism, it is possible to retrieve path data from each item in every way
+currently provided by L<File::Find>, without retaining global state to do so.
+As a reference to the corresponding item's inode object is passed, there is no
+need to perform a $fs->stat() call to further inspect the item.
+
+When called with an $options argument, specified in the form of an anonymous
+HASH, the following flags (whose values are set nonzero) are honored:
+
+=over
+
+=item C<follow>
+
+Any symlinks found along the way are resolved; if the paths they resolve to are
+those of directories, then further descent will be made into said directories.
+
+=back
+
+=back
+
+=cut
 sub find {
     my $self = shift;
     my $callback = shift;
