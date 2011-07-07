5c932aa054e404f69fc5121ef8d4f73f4aa17f75
Erin Schoenhals <erin@cpanel.net>
2011-07-07 16:43:32 -0500

    Use 'inode', not 'node' as internal designation

--- lib/Filesys/POSIX/Real/Directory.pm
+++ lib/Filesys/POSIX/Real/Directory.pm
@@ -16,7 +16,7 @@ sub from_disk {
 
     return bless {
         'path'     => $path,
-        'node'     => $inode,
+        'inode'    => $inode,
         'mtime'    => 0,
         'overlays' => {},
         'skipped'  => {},
@@ -63,8 +63,8 @@ sub _sync_member {
         $self->{'members'}->{$name} = Filesys::POSIX::Real::Inode->from_disk(
             $subpath,
             'st_info' => \@st,
-            'dev'     => $self->{'node'}->{'dev'},
-            'parent'  => $self->{'node'}
+            'dev'     => $self->{'inode'}->{'dev'},
+            'parent'  => $self->{'inode'}
         );
     }
 }
@@ -115,7 +115,7 @@ sub delete {
     }
 
     my $now = time;
-    @{ $self->{'node'} }{qw/mtime ctime/} = ( $now, $now );
+    @{ $self->{'inode'} }{qw/mtime ctime/} = ( $now, $now );
 
     my $inode = $self->{'members'}->{$name};
     delete $self->{'members'}->{$name};
